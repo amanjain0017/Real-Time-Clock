33f4e39071bc0d44f7a2ba4d77b90049ea5f9bd7
Aman Jain <jain.amanjain01@gmail.com>
2023-09-20 18:01:54 +0530

    first commit

---
+++ timer.js
@@ -0,0 +1,17 @@
+
+let a;
+let date;
+let time;
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+
+setInterval(() => {
+    
+    a = new Date();
+    
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    date = a.toLocaleDateString(undefined, options);
+    
+    document.getElementById('time').innerHTML = time + " <br>on the " +  date;
+}, 1000);
